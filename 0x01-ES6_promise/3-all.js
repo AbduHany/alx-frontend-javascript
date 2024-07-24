a326d7e85f4883b9b9ad89c9c4ad8be7199fa50d
AbduHany <abdu.hany@gmail.com>
2024-07-24 14:04:43 +0000

    Modify 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((value) => { process.stdout.write(`${value.body} `); }, () => { console.log('Signup system offline'); });
-  createUser().then((value) => { console.log(`${value.firstName} ${value.lastName}`); }, () => { console.log('Signup system offline'); });
+  Promise.all([uploadPhoto(), createUser()]).then((val) => { console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
 }
