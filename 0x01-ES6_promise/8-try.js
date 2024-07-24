da117b1dfc05bd64424f34fbdf13a86e9ec5013c
AbduHany <abdu.hany@gmail.com>
2024-07-24 19:12:34 +0000

    Modify 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (!denominator) throw (new Error('cannot divide by 0'));
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  else {
     return (numerator / denominator);
-  } catch (error) {
-    return error;
   }
 }
