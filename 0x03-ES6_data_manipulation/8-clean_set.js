1c74c01b88ff550c5f98be4673da5b72390ddc8c
AbduHany <abdu.hany@gmail.com>
2024-07-30 01:40:19 +0300

    Modify 8-clean.js: add edge cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   ) return '';
   const words = [];
   for (const word of set) {
-    if (word.startsWith(startString)) {
+    if (word && word.startsWith(startString)) {
       words.push(word.slice(startString.length));
     }
   }
