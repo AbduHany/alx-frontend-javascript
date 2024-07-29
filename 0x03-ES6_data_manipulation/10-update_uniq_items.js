079d58b64fed97fb80c90d596ee317ab609a58a0
AbduHany <abdu.hany@gmail.com>
2024-07-30 01:54:44 +0300

    Modify 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(map) {
-  for (const [key, value] of map.entries()) {
-    if (value === 1) {
-      try {
+  try {
+    for (const [key, value] of map.entries()) {
+      if (value === 1) {
         map.set(key, 100);
-      } catch (err) {
-        throw new Error('Cannot process');
       }
     }
+    return map;
+  } catch (err) {
+    throw new Error('Cannot process');
   }
-  return map;
 }
