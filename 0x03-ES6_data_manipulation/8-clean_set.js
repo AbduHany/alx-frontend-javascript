d8c8b0f085beaa1794964f2e85cb4186ee996f9f
AbduHany <abdu.hany@gmail.com>
2024-07-30 01:38:42 +0300

    Modify 8-clean.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (
+    startString === '' || typeof startString !== 'string' || typeof set !== 'object'
+  ) return '';
   const words = [];
   for (const word of set) {
     if (word.startsWith(startString)) {
